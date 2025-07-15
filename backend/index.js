726a58ead0b7a49129ed61ff7729fd5ba35bab80
Yanwen Huang <huangyw28@mail2.sysu.edu.cn>
2025-07-15 15:36:22 +0800

    commit 3.0

--- backend/index.js
+++ backend/index.js
@@ -49,6 +49,6 @@ app.post('/api/route/straight', async (req, res) => {
   }
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0',() => {
   console.log(`Backend server running on http://localhost:${PORT}`);
 }); 
